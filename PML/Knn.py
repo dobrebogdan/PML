20274c9ab53d0d45b01df32965660228311f1310
Dobre Bogdan <dobrebogdan98@gmail.com>
2020-11-24 00:45:20 +0200

    Minimal idea

--- PML/Knn.py
+++ PML/Knn.py
@@ -1,20 +1,46 @@
 class Knn_classifier:
 
-    def __init__(self, train_images, train_labels):
-        self.train_images = train_images
+    def __init__(self, train_data, train_labels, validation_data, validation_labels):
+        self.train_data = train_data
         self.train_labels = train_labels
+        self.validation_data = validation_data
+        self.validation_labels = validation_labels
 
     # a. Write the classify_image(self, test_image, num_neighbors=3, metric='l2') function in order to classify 'test_image'
     # example using the k-NN method with 'num_neighbors' neighbors and 'metric' distance.
-    def classify_image(self, test_image, num_neighbors=3, metric='l2'):
-        # write your code here
-        pass
 
-    # b. Write the classify_images(self, test_images, num_neighbors=3, metric='l2') function in order to predict the labels of
-    # the test images.
-    def classify_images(self):
+    def compute_distance(self, tweet1, tweet2):
+        return abs(len(tweet1) - len(tweet2))
+
+    def classify_tweet(self, tweet, num_neighbors=5):
+        distances = [(0, 0, 0)] * len(self.train_data)
+        for i in range(0, len(self.train_data)):
+            distances[i] = (self.compute_distance(tweet, self.train_data[i]), self.train_labels[i][0], self.train_labels[i][1])
+        distances = sorted(distances)
+        # print(distances)
+        meanLong = 0.0
+        meanLat = 0.0
+        for i in range(0, num_neighbors):
+            meanLong += float(distances[i][1])
+            meanLat += float(distances[i][2])
+        meanLong /= num_neighbors
+        meanLat /= num_neighbors
+        return meanLong, meanLat
+
+
+
+
+
+    def classify_tweets(self):
         # write your code here
-        pass
+        predictions = [(0.0, 0.0)] * len(self.validation_data)
+        for i in range(0, len(self.validation_data)):
+            predictions[i] = self.classify_tweet(self.validation_data[i])
+            if(i %10 ==  0):
+                print("STEP " + str(i))
+        print(predictions)
+
+
 
     # c. Define a function to compute the accurracy score given the predicted labels and the ground-truth labels.
     def accuracy_score(self):
--- PML/main.py
+++ PML/main.py
@@ -6,12 +6,20 @@ from Knn import Knn_classifier
 # Press the green button in the gutter to run the script.
 train_data = []
 train_labels = []
+train_data_dict = {}
 with open("training.txt") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     for row in csv_reader:
         train_data.append(row[3])
-        train_labels.append([row[1], row[2]])
+        train_labels.append((row[1], row[2]))
 
+        train_data_dict[(row[1], row[2])] = train_data_dict.get((row[1], row[2]), "") + " " + row[3]
+print(len(train_data_dict))
+print(len(train_data))
+
+for pair in train_data_dict.items():
+    print(pair)
+    print("---------------------------------------------------")
 validation_data = []
 validation_labels = []
 with open("validation.txt") as csv_file:
@@ -20,10 +28,14 @@ with open("validation.txt") as csv_file:
         validation_data.append(row[3])
         validation_labels.append([row[1], row[2]])
 
+
 train_data = np.array(train_data)
 train_labels = np.array(train_labels)
 
 validation_data = np.array(validation_data)
 validation_labels = np.array(validation_labels)
 
-knn_classifier = Knn_classifier(train_data, train_labels)
+knn_classifier = Knn_classifier(train_data, train_labels, validation_data, validation_labels)
+#knn_classifier.classify_tweets()
+
+# sunt mai multe tweeturi de la aceleasi persoane. Cate? Este un fenomen raspandit?
